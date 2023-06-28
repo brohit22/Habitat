0369239b9a770ef2cfc43048e6ac836298b335d7
Mr Mic <127715071+brohit22@users.noreply.github.com>
2023-06-28 10:48:30 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,5 @@
 const mongoose = require("mongoose");
 
-// mongoose
-//   .connect("mongodb+srv://rahul:rahul001@cluster0.uzoqy7q.mongodb.net/habit_tracker")
-//   .then(() => console.log("Mongodb Connected"))
-//   .catch((e) => console.log(e));
-
 mongoose
   .connect("mongodb://127.0.0.1/myHabitat", {
     useNewUrlParser: true,
